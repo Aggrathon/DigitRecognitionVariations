adf0eb5782654e7116b0e8e408b48bae77b46321
Aggrathon <antonbjo@gmail.com>
2017-09-11 11:04:13 +0300

    Non convolutional network with Tensorflow

--- tf_nn.py
+++ tf_nn.py
@@ -1,12 +1,13 @@
 """
-    This script contains a convolutional neural network built with tensorflow
+    This script contains a neural network built with tensorflow
 """
 import sys
+import numpy as np
 import tensorflow as tf
 from data import get_test_set, get_training_set
 from utils import tf_learning_rate_scaling
 
-FOLDER = 'tf_cnn'
+FOLDER = 'tf_nn'
 
 def model_fn(features, labels, mode):
     """
@@ -14,12 +15,7 @@ def model_fn(features, labels, mode):
     """
     training = (mode is tf.estimator.ModeKeys.TRAIN)
     prev_layer = features['img']
-    for i, (size, kernel) in enumerate([(32, 7), (64, 5), (96, 3)]):
-        prev_layer = tf.layers.conv2d(prev_layer, size, kernel, 1, 'same', activation=tf.nn.relu, name='conv%d'%i)
-        prev_layer = tf.layers.max_pooling2d(prev_layer, 3, 2, 'valid', name='pool%d'%i)
-        prev_layer = tf.layers.batch_normalization(prev_layer, name="norm%d"%i, training=training)
-    prev_layer = tf.contrib.layers.flatten(prev_layer)
-    for i, size in enumerate([128, 128]):
+    for i, size in enumerate([1024, 1024, 512, 256, 128]):
         prev_layer = tf.layers.dense(prev_layer, size, activation=tf.nn.relu, name='fc%d'%i)
         prev_layer = tf.layers.dropout(prev_layer, 0.3, training=training, name='dropout%d'%i)
     logit = tf.layers.dense(prev_layer, 10, name='logit')
@@ -30,7 +26,7 @@ def model_fn(features, labels, mode):
         loss = tf.losses.softmax_cross_entropy(tf.one_hot(labels, 10), logit)
         if training:
             global_step = tf.train.get_global_step()
-            learning_rate = tf_learning_rate_scaling([(0, 1e-3), (300, 1e-4), (1000, 1e-5), (3000, 1e-6), (8000, 1e-7)], global_step)
+            learning_rate = tf_learning_rate_scaling([(0, 1e-3), (1000, 1e-4), (5000, 1e-5), (10000, 1e-6), (20000, 1e-7)], global_step)
             trainer = tf.train.AdamOptimizer(learning_rate)
             update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
             with tf.control_dependencies(update_ops):
@@ -54,9 +50,11 @@ def evaluate(train=False, epochs=None):
     model = tf.estimator.Estimator(model_fn, FOLDER)
     if train:
         img, lab = get_training_set()
-        input_fn = tf.estimator.inputs.numpy_input_fn(dict(img=img), lab, 128, epochs, True, 6000)
+        img.shape = img.shape[0], np.product(img.shape[1:])
+        input_fn = tf.estimator.inputs.numpy_input_fn(dict(img=img), lab, 192, epochs, True, 6000)
         model.train(input_fn)
     img, lab = get_test_set()
+    img.shape = img.shape[0], np.product(img.shape[1:])
     return model.evaluate(tf.estimator.inputs.numpy_input_fn(dict(img=img), lab, 1000, 1, True, 10000))
 
 
---
+++ utils.py
@@ -0,0 +1,25 @@
+"""
+    This script contains some useful functions
+"""
+
+import tensorflow as tf
+
+
+def tf_learning_rate_scaling(points, global_step):
+    assert len(points) > 1
+    global_step = tf.to_float(global_step)
+    rate = tf.constant(0.0, tf.float32)
+    for i in range(1, len(points)):
+        x1, y1 = points[i-1]
+        x2, y2 = points[i]
+        assert x1 < x2
+        assert y1 > y2
+        a = (y1-y2)/(x1-x2)
+        b1 = y1 - a * x1
+        b2 = y2 - a * x2
+        assert abs(b1-b2) < 1e-4
+        b = (b1+b2)*0.5
+        rate = tf.maximum(rate, tf.add(tf.multiply(global_step, a), b))
+    rate = tf.maximum(rate, points[-1][1])
+    return rate
+
