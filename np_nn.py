0acd538afb3a43939fd8001431f8ef4722a1f945
Aggrathon <antonbjo@gmail.com>
2017-09-14 02:14:38 +0300

    Saving and loading of numpy model

--- np_nn.py
+++ np_nn.py
@@ -3,9 +3,12 @@
 """
 
 import sys
+import os
+from timeit import default_timer as timer
 import numpy as np
 from data import get_test_set, get_training_set
 
+FOLDER = 'np_nn'
 
 def relu(x):
     return np.maximum(0.0, x)
@@ -28,7 +31,7 @@ def softmax_prime(x):
 
 class Layer():
 
-    def __init__(self, input_num, output_num, activation=relu, prime=relu_prime):
+    def __init__(self, input_num, output_num, name, activation=relu, prime=relu_prime):
         self.bias = np.zeros((output_num))
         glorot = np.sqrt(6/(input_num+output_num))
         self.weights = np.random.uniform(-glorot, glorot, (output_num, input_num))
@@ -37,6 +40,8 @@ class Layer():
             self.prime = lambda z: 1
         else:
             self.prime = prime
+        self.name = name
+        self.load()
 
     def forward(self, prev_layer, dropout=0.3):
         z = np.dot(self.weights, prev_layer) + self.bias
@@ -52,6 +57,21 @@ class Layer():
             return result, z
         else:
             return z, z
+    
+    def save(self):
+        try:
+            os.makedirs(FOLDER, exist_ok=True)
+            np.save(os.path.join(FOLDER, self.name+'_weights.npy'), self.weights)
+            np.save(os.path.join(FOLDER, self.name+'_bias.npy'), self.bias)
+        except:
+            pass
+    
+    def load(self):
+        try:
+            self.weights = np.load(os.path.join(FOLDER, self.name+'_weights.npy'))
+            self.bias = np.load(os.path.join(FOLDER, self.name+'_bias.npy'))
+        except:
+            pass
 
 
 class Network():
@@ -59,12 +79,12 @@ class Network():
     def __init__(self, input_size=28*28, output_size=10):
         self.layers = []
         size = input_size
-        for i in [1024, 512, 128]:
-            self.layers.append(Layer(size, i))
-            size = i
-        self.layers.append(Layer(size, output_size, softmax, softmax_prime))
+        for i, s in enumerate([1024, 512, 128]):
+            self.layers.append(Layer(size, s, 'fc%d'%i))
+            size = s
+        self.layers.append(Layer(size, output_size, 'output', softmax, softmax_prime))
 
-    def sgd(self, epochs=30, batch_size=100, learning_rate=0.1):
+    def sgd(self, epochs=30, batch_size=100, learning_rate=0.05):
         img, lab = get_training_set()
         img.shape = img.shape[0], np.prod(img.shape[1:])
         learning_rate /= batch_size
@@ -74,17 +94,27 @@ class Network():
                 np.random.shuffle(img)
                 np.random.set_state(rng)
                 np.random.shuffle(lab)
-                print("Starting epoch:", i)
+                time = timer()
+                loss = 0.0
                 for start in range(0, img.shape[0]-batch_size+1, batch_size):
-                    n_b, n_w, loss = self.backprop(img[start:start+batch_size], lab[start:start+batch_size])
+                    n_b, n_w, l = self.backprop(img[start:start+batch_size], lab[start:start+batch_size])
                     for layer, b, w in zip(self.layers, n_b, n_w):
                         layer.bias = layer.bias - learning_rate * b
-                        layer.weights = layer.weights - learning_rate * w
-                    loss /= batch_size
-                    print('loss:', loss)
+                        layer.weights = layer.weights - learning_rate * w - (1e-8)*layer.weights
+                    loss += l
+                    count = start//batch_size+1
+                    if count%10 == 0:
+                        print('[Epoch %d - %d / %d]   Accuracy: %.3f   (%.2f s)'%(i, count, lab.shape[0]//batch_size, loss/(batch_size*10), (timer()-time)/10))
+                        time = timer()
+                        loss = 0.0
                 learning_rate *= 0.7
+                self.evaluate()
+            for l in self.layers:
+                l.save()
         except KeyboardInterrupt:
             print('Aborting...')
+            for l in self.layers:
+                l.save()
 
     def backprop(self, x_list, y_list):
         n_b = [np.zeros_like(l.bias) for l in self.layers]
@@ -121,7 +151,7 @@ class Network():
                 prev, _ = layer.forward(prev, 0)
             if np.argmax(prev) == lab[i]:
                 correct += 1
-        print(correct, '/', lab.shape[0])
+        print('Evaluation:', correct, '/', lab.shape[0], 'correct')
 
 
 def _test_forward():
